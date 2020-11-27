1317fcc2a7b77a3d89c160ec17736448ce44a9c4
KapteinKS <sander.toien@outlook.com>
2020-11-27 13:26:58 +0100

    small optimization

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,12 @@ public class Main {
             exp += calculateEXP(lvl);
             double calculated = calculateLVL(exp);
             battles++;
+            moreBattles++;
             if(calculated > lvl){
                 lvl = calculated;
-                moreBattles = battles - lvlReachedAt;
                 lvlReachedAt = battles;
                 System.out.println("Level " + lvl + " reached after " + battles + " battles, " + moreBattles + " more battles than last lvl");
+                moreBattles = 0;
             }
         }
     }
@@ -24,197 +25,194 @@ public class Main {
     }
 
     public static double calculateLVL (long exp){
-        double lvl = 6;
         if ( exp < 314 && exp >= 236){
-            lvl = 7;
+            return 7;
         } else if (exp < 419 && exp >= 314){
-            lvl = 8;
+            return 8;
         } else if (exp < 560 && exp >= 419){
-            lvl = 9;
+            return 9;
         } else if (exp < 742 && exp >= 560){
-            lvl = 10;
+            return 10;
         } else if (exp < 973 && exp >= 742){
-            lvl = 11;
+            return 11;
         } else if (exp < 1261 && exp >= 973){
-            lvl = 12;
+            return 12;
         } else if (exp < 1612 && exp >= 1261){
-            lvl = 13;
+            return 13;
         } else if (exp < 2035 && exp >= 1612){
-            lvl = 14;
+            return 14;
         } else if (exp < 2535 && exp >= 2035){
-            lvl = 15;
+            return 15;
         } else if (exp < 3120 && exp >= 2535){
-            lvl = 16;
+            return 16;
         } else if (exp < 3798 && exp >= 3120){
-            lvl = 17;
+            return 17;
         } else if (exp < 4575 && exp >= 3798){
-            lvl = 18;
+            return 18;
         } else if (exp < 5460 && exp >= 4575){
-            lvl = 19;
+            return 19;
         } else if (exp < 6458 && exp >= 5460){
-            lvl = 20;
+            return 20;
         } else if (exp < 7577 && exp >= 6458){
-            lvl = 21;
+            return 21;
         } else if (exp < 8825 && exp >= 7577){
-            lvl = 22;
+            return 22;
         } else if (exp < 10208 && exp >= 8825){
-            lvl = 23;
+            return 23;
         } else if (exp < 11735 && exp >= 10208){
-            lvl = 24;
+            return 24;
         } else if (exp < 13411 && exp >= 11735){
-            lvl = 25;
+            return 25;
         } else if (exp < 15244 && exp >= 13411){
-            lvl = 26;
+            return 26;
         } else if (exp < 17242 && exp >= 15244){
-            lvl = 27;
+            return 27;
         } else if (exp < 19411 && exp >= 17242){
-            lvl = 28;
+            return 28;
         } else if (exp < 21760 && exp >= 19411){
-            lvl = 29;
+            return 29;
         } else if (exp < 24294 && exp >= 21760){
-            lvl = 30;
+            return 30;
         } else if (exp < 27021 && exp >= 24294){
-            lvl = 31;
+            return 31;
         } else if (exp < 29949 && exp >= 27021){
-            lvl = 32;
+            return 32;
         } else if (exp < 33084 && exp >= 29949){
-            lvl = 33;
+            return 33;
         } else if (exp < 36435 && exp >= 33084){
-            lvl = 34;
+            return 34;
         } else if (exp < 40007 && exp >= 36435){
-            lvl = 35;
+            return 35;
         } else if (exp < 43808 && exp >= 40007){
-            lvl = 36;
+            return 36;
         } else if (exp < 47846 && exp >= 43808){
-            lvl = 37;
+            return 37;
         } else if (exp < 52127 && exp >= 47846){
-            lvl = 38;
+            return 38;
         } else if (exp < 56660 && exp >= 52127){
-            lvl = 39;
+            return 39;
         } else if (exp < 61450 && exp >= 56660){
-            lvl = 40;
+            return 40;
         } else if (exp < 66505 && exp >= 61450){
-            lvl = 41;
+            return 41;
         } else if (exp < 71833 && exp >= 66505){
-            lvl = 42;
+            return 42;
         } else if (exp < 77440 && exp >= 71833){
-            lvl = 43;
+            return 43;
         } else if (exp < 83335 && exp >= 77440){
-            lvl = 44;
+            return 44;
         } else if (exp < 89523 && exp >= 83335){
-            lvl = 45;
+            return 45;
         } else if (exp < 96012 && exp >= 89523){
-            lvl = 46;
+            return 46;
         } else if (exp < 102810 && exp >= 96012){
-            lvl = 47;
+            return 47;
         } else if (exp < 109923 && exp >= 102810){
-            lvl = 48;
+            return 48;
         } else if (exp < 117360 && exp >= 109923){
-            lvl = 49;
+            return 49;
         } else if (exp < 125126 && exp >= 117360){
-            lvl = 50;
+            return 50;
         } else if (exp < 133229 && exp >= 125126){
-            lvl = 51;
+            return 51;
         } else if (exp < 141677 && exp >= 133229){
-            lvl = 52;
+            return 52;
         } else if (exp < 150476 && exp >= 141677){
-            lvl = 53;
+            return 53;
         } else if (exp < 159635 && exp >= 150476){
-            lvl = 54;
+            return 54;
         } else if (exp < 169159 && exp >= 159635){
-            lvl = 55;
+            return 55;
         } else if (exp < 179056 && exp >= 169159){
-            lvl = 56;
+            return 56;
         } else if (exp < 189334 && exp >= 179056){
-            lvl = 57;
+            return 57;
         } else if (exp < 199999 && exp >= 189334){
-            lvl = 58;
+            return 58;
         } else if (exp < 211060 && exp >= 199999){
-            lvl = 59;
+            return 59;
         } else if (exp < 222522 && exp >= 211060){
-            lvl = 60;
+            return 60;
         } else if (exp < 234393 && exp >= 222522){
-            lvl = 61;
+            return 61;
         } else if (exp < 246681 && exp >= 234393){
-            lvl = 62;
+            return 62;
         } else if (exp < 259392 && exp >= 246681){
-            lvl = 63;
+            return 63;
         } else if (exp < 272535 && exp >= 259392){
-            lvl = 64;
+            return 64;
         } else if (exp < 286115 && exp >= 272535){
-            lvl = 65;
+            return 65;
         } else if (exp < 300140 && exp >= 286115){
-            lvl = 66;
+            return 66;
         } else if (exp < 314618 && exp >= 300140){
-            lvl = 67;
+            return 67;
         } else if (exp < 329555 && exp >= 314618){
-            lvl = 68;
+            return 68;
         } else if (exp < 344960 && exp >= 329555){
-            lvl = 69;
+            return 69;
         } else if (exp < 360838 && exp >= 344960){
-            lvl = 70;
+            return 70;
         } else if (exp < 377197 && exp >= 360838){
-            lvl = 71;
+            return 71;
         } else if (exp < 394045 && exp >= 377197){
-            lvl = 72;
+            return 72;
         } else if (exp < 411388 && exp >= 394045){
-            lvl = 73;
+            return 73;
         } else if (exp < 429235 && exp >= 411388){
-            lvl = 74;
+            return 74;
         } else if (exp < 447591 && exp >= 429235){
-            lvl = 75;
+            return 75;
         } else if (exp < 466464 && exp >= 447591){
-            lvl = 76;
+            return 76;
         } else if (exp < 485862 && exp >= 466464){
-            lvl = 77;
+            return 77;
         } else if (exp < 505791 && exp >= 485862){
-            lvl = 78;
+            return 78;
         } else if (exp < 526260 && exp >= 505791){
-            lvl = 79;
+            return 79;
         } else if (exp < 547274 && exp >= 526260){
-            lvl = 80;
+            return 80;
         } else if (exp < 568841 && exp >= 547274){
-            lvl = 81;
+            return 81;
         } else if (exp < 590969 && exp >= 568841){
-            lvl = 82;
+            return 82;
         } else if (exp < 613664 && exp >= 590969){
-            lvl = 83;
+            return 83;
         } else if (exp < 636935 && exp >= 613664){
-            lvl = 84;
+            return 84;
         } else if (exp < 660787 && exp >= 636935){
-            lvl = 85;
+            return 85;
         } else if (exp < 685228 && exp >= 660787){
-            lvl = 86;
+            return 86;
         } else if (exp < 710266 && exp >= 685228){
-            lvl = 87;
+            return 87;
         } else if (exp < 735907 && exp >= 710266){
-            lvl = 88;
+            return 88;
         } else if (exp < 762160 && exp >= 735907){
-            lvl = 89;
+            return 89;
         } else if (exp < 789030 && exp >= 762160){
-            lvl = 90;
+            return 90;
         } else if (exp < 816525 && exp >= 789030){
-            lvl = 91;
+            return 91;
         } else if (exp < 844653 && exp >= 816525){
-            lvl = 92;
+            return 92;
         } else if (exp < 873420 && exp >= 844653){
-            lvl = 93;
+            return 93;
         } else if (exp < 902835 && exp >= 873420){
-            lvl = 94;
+            return 94;
         } else if (exp < 932903 && exp >= 902835){
-            lvl = 95;
+            return 95;
         } else if (exp < 963632 && exp >= 932903){
-            lvl = 96;
+            return 96;
         } else if (exp < 995030 && exp >= 963632){
-            lvl = 97;
+            return 97;
         } else if (exp < 1027103 && exp >= 995030){
-            lvl = 98;
+            return 98;
         } else if (exp < 1059860 && exp >= 1027103){
-            lvl = 99;
-        } else if (exp >= 1059860){
-            lvl = 100;
+            return 99;
+        } else {
+            return 100;
         }
-
-        return lvl;
     }
 }
